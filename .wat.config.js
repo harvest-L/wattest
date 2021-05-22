843a3182a40a3e1ec5e852d332730fb665b289c4
liupengfei <liupengfei@duiba.com.cn>
2021-05-22 21:55:34 +0800

    测试

--- .wat.config.js
+++ .wat.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  files: [
-    'pageage.json',
-    'index.js'
+  prompt: [
+    {
+      name: 'vueVersion',
+      type: 'input',
+      default: '3'
+    },
+    {
+      name: 'doesCompile',
+      type: 'confirm'
+    },
+    {
+      name: 'useloadsh',
+      type: 'confirm'
+    }
   ]
 }

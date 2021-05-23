b723ddf27a0bf85b56caa96800b14bb1b7a79d62
liupengfei <liupengfei@duiba.com.cn>
2021-05-23 18:32:00 +0800

    模版测试

--- .wat.config.js
+++ .wat.config.js
@@ -1,4 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-23 03:22:32
+ * @LastEditTime: 2021-05-23 03:22:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /wattest/.wat.config.js
+ */
 module.exports = {
+  name: 'watTest',
+  author: '0.0.1',
+  tag:'vue',
+  git: 'git@github.com:harvest-L/wattest.git',
   prompt: [
     {
       name: 'vueVersion',
